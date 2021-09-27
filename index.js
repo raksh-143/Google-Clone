493601800188fafacd914fdb452ad87011714504
Rakshitha B A <rakshitha.aruna@gmail.com>
2021-09-27 10:53:04 +0530

    Project UI done

---
+++ index.js
@@ -0,0 +1,8 @@
+function signin(){
+    window.open('https://accounts.google.com/ServiceLogin/signinchooser?hl=en&passive=true&continue=https%3A%2F%2Fwww.google.co.in%2F%3Fgws_rd%3Dssl&ec=GAZAmgQ&flowName=GlifWebSignIn&flowEntry=ServiceLogin','_self');
+}
+$(document).ready(function(){
+    $("img").click(function(){
+        window.open('https://www.google.co.in/','_self');
+    })
+})
