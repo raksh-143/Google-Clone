9b78a4ea5175b177e800decedd15861ef76cd564
Rakshitha B A <rakshitha.aruna@gmail.com>
2021-09-29 14:44:07 +0530

    Removed deprecated code

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function signin(){
     window.open('https://accounts.google.com/ServiceLogin/signinchooser?hl=en&passive=true&continue=https%3A%2F%2Fwww.google.co.in%2F%3Fgws_rd%3Dssl&ec=GAZAmgQ&flowName=GlifWebSignIn&flowEntry=ServiceLogin','_self');
 }
-$(document).ready(function(){
-    $("img").click(function(){
+$(function(){
+    $(document).on("click","img",function(){
         window.open('https://www.google.co.in/','_self');
     })
-    $("#lucky").click(function(){
+    $(document).on("click","#lucky",function(){
         window.open('https://www.google.com/doodles','_self');
     })
 })
